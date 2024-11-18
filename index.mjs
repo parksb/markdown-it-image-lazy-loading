ed4580edb0e0849ea25ab5264d09b6f41cfab72f
parksb <parkgds@gmail.com>
2024-11-18 15:50:42 +0900

    feat: exclude first image

--- index.mjs
+++ index.mjs
@@ -3,10 +3,13 @@ import path from 'node:path';
 
 function lazy_loading_plugin(md, mdOptions) {
   var defaultImageRenderer = md.renderer.rules.image;
+  var count = 0;
 
   md.renderer.rules.image = function (tokens, idx, options, env, self) {
     var token = tokens[idx];
-    token.attrSet('loading', 'lazy');
+    if (count > 0) {
+      token.attrSet('loading', 'lazy');
+    }
 
     if (mdOptions && mdOptions.decoding === true) {
       token.attrSet('decoding', 'async');
@@ -26,6 +29,8 @@ function lazy_loading_plugin(md, mdOptions) {
       token.attrSet('width', dimensions.width);
       token.attrSet('height', dimensions.height);
     }
+
+    count += 1;
 
     return defaultImageRenderer(tokens, idx, options, env, self);
   };
